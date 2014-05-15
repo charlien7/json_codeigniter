52a390fb4d44ce22275b71bc08c556a532fced0a
Carlos Jovel <cm.jovel@gmail.com>
2014-05-15 17:28:08 -0600

    Testing data in controller

--- application/controllers/jsonci.php
+++ application/controllers/jsonci.php
@@ -4,37 +4,25 @@ Class Jsonci extends CI_CONTROLLER
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->model('jsonci_model');
-		$this->load->helper('url');
-		$this->load->database('json');
-		$this->config->set_item('base_url','http://localhost/json_codeigniter/') ;
+		
 	}
 
-	/*public function index()
+
+	public function index()
 	{
+		$this->load->model('jsonci_model');
+	    $some_data = $this->jsonci_model->provincias();
+	    $some_data = json_encode($some_data);
 
-		$data = array('titulo' => 'Titulo');
-		$this->load->view('json_view',$data);
-
-	}*/
+	    echo '<pre>'. print_r($some_data, 1).'</pre>'; exit();
 
 
+	    $data = array (
+	            'sodata' => $some_data
+	    );
 
-public function index()
-{
-    $some_data = $this->jsonci_model->provincias();
-    $some_data = json_encode($some_data);
-    $data = array (
-            'some_data' => $some_data
-    );
-        $this->load->view('json_view',$data);
+	    
+	    $this->load->view('json_view', $data);
 
+	}
 }
-
-
-
-
-
-	
-}
-//end controller
