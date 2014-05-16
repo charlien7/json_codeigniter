f54df51dc711f6cbc631289206f18d7e33710e35
Carlos Jovel <cm.jovel@gmail.com>
2014-05-15 22:50:03 -0600

    Add foundation

---
+++ application/views/listar.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Listar</title>
+	<link rel="stylesheet" href="<?php asset_url(); ?>css/vendor/normalize.css">
+	<link rel="stylesheet" href="<?php asset_url(); ?>css/vendor/foundation.min.css">
+	<script src="<?php asset_url(); ?>js/vendor/modernizr.js"></script>
+</head>
+<body>
+	<div class="contain-to-grid">
+		<nav class="top-bar" data-topbar>
+			<ul class="title-area">
+				<li class="name">
+					<h1><a href="#">Site Name</a></h1>
+				</li>
+				<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+			</ul>
+			<section class="top-bar-section">
+				<!-- Right Nav Section -->
+				<ul class="right">
+					<li class="active"><a href="#">Menu Option</a></li>
+					<li><a href="#">Menu Item</a></li>
+					<li><a href="#">Another Item</a></li>
+				</ul>
+			</section>
+		</nav>
+	</div>
+	<div class="row">
+		<div class="columns">
+			<h1>Foundation, baby!</h1>
+		</div>
+	</div>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="<?php asset_url(); ?>js/vendor/jquery.js"><\/script>')</script>
+    <script src="<?php asset_url(); ?>js/vendor/foundation.min.js"></script>
+    <script src="<?php asset_url(); ?>js/vendor/foundation.topbar.js"></script>
+    <script src="<?php asset_url(); ?>js/vendor/foundation.dropdown.js"></script>
+    <script>$(document).foundation();</script>
+    <script src="<?php asset_url(); ?>js/main.js"></script>
+</body>
+</html>
